e9bac29863e762cf53fe2989343a79a9bdf428f4
morishita.hiromitsu <morishita.hiromitsu@actindi.net>
2018-03-06 00:59:21 +0900

    move private functions out of Object

--- src/toggl.js
+++ src/toggl.js
@@ -1,3 +1,69 @@
+/**
+ * TogglのAPIを実行する
+ * @param  {[type]} url APIのエンドポイントURL＋クエリパラメータ
+ * @return {[type]}     APIのレスポンス
+ */
+function callTogglApi(url) {
+  const togglKey = Props.get('TOGGL_API_TOKEN');
+  const authToken = Utilities.base64Encode(`${togglKey}:api_token`);
+  const headers = { Authorization: `Basic ${authToken}` };
+  const response = UrlFetchApp.fetch(url, { headers });
+  return response;
+}
+
+/**
+ * レポート（詳細）を取得する
+ * @param  {Integer} workplaceId ワークプレイスID
+ * @param  {String} since       取得開始日（YYYY-MM-DD）
+ * @param  {String} until       取得最終日（YYYY-MM-DD）
+ * @param  {Number} [page=1]    取得ページ番号
+ * @return {Array[Obejct]}      Togglのレポートオブジェクト
+ */
+function fetchReport(workplaceId, since, until, page = 1) {
+  const url = Utilities.formatString(
+    'https://toggl.com/reports/api/v2/details?workspace_id=%s&since=%s&until=%s&page=%s&user_agent=toggl2rm',
+    workplaceId,
+    since,
+    until,
+    page,
+  );
+  const response = callTogglApi(url);
+  const content = JSON.parse(response.getContentText());
+  const reportJson = content.data;
+  const hasNext = (content.per_page <= reportJson.length);
+  Logger.log('%s, %s, %s', reportJson.length, hasNext, page);
+  return { reportJson, hasNext };
+}
+
+/**
+ * レポート（詳細）を扱いやすくパースする。
+ * レポートオブジェクトから次の値の配列に変換する
+ *   - report.id : TooglのタスクID
+ *   - ticketNo :  Redmineのチケット 番号
+ *   - startDate : 作業の日付
+ *   - duration : 作業時間（Hour）
+ *   - report.tags.join('、'): 作業のメモ（Togglのタグ）
+ *   - report.description: Togglのタスク名
+ * @param  {Array[Object]} reportJson Togglのレポートオブジェクトの配列
+ * @return {Array[][]}     必要な値の配列のリスト
+ */
+function parseReportData(reportJson) {
+  const ticketNoIndex = 1;
+  const parsedReport = reportJson.map((report) => {
+    const startDate = /^([0-9]{4}-[0-9]{2}-[0-9]{2})T.+$/.exec(report.start)[1];
+    const duration = Math.round(report.dur / (60 * 60 * 10)) / 100;
+    let ticketNo = '';
+    if (/^#/.test(report.description)) {
+      ticketNo = /^#([0-9]+):?[\s][\S]+/.exec(report.description)[ticketNoIndex];
+    } else {
+      return null;
+    }
+    const formattedRepo = [report.id, ticketNo, startDate, duration, report.tags.join('、'), report.description];
+    return formattedRepo;
+  }).filter(elm => (elm != null));
+  return parsedReport;
+}
+
 const Toggl = {
   /**
    * ワークスペースを取得する
@@ -5,7 +71,7 @@ const Toggl = {
    */
   getWorkspaces() {
     const url = 'https://www.toggl.com/api/v8/workspaces';
-    const response = this._callTogglApi(url);
+    const response = callTogglApi(url);
     const content = JSON.parse(response.getContentText());
     Logger.log(content);
     const workspaces = content.map(ws => ({ id: ws.id, name: ws.name }));
@@ -24,7 +90,7 @@ const Toggl = {
   getAllReport(workplaceId, year, month) {
     const period = Utils.getPeriod(year, month);
     const reportJson = this.fetchAllReport(workplaceId, period.since, period.until);
-    return this._parseReportData(reportJson);
+    return parseReportData(reportJson);
   },
 
   /**
@@ -40,7 +106,7 @@ const Toggl = {
     let hasNext = true;
     let report = [];
     while (hasNext) {
-      const result = this._fetchReport(workplaceId, since, until, page);
+      const result = fetchReport(workplaceId, since, until, page);
       const reportJson = result.reportJson;
       hasNext = result.hasNext;
       Utilities.sleep(1500); // APIのrate limitを避けるため
@@ -48,72 +114,6 @@ const Toggl = {
       page += 1;
     }
     return report;
-  },
-
-  /**
-   * レポート（詳細）を取得する
-   * @param  {Integer} workplaceId ワークプレイスID
-   * @param  {String} since       取得開始日（YYYY-MM-DD）
-   * @param  {String} until       取得最終日（YYYY-MM-DD）
-   * @param  {Number} [page=1]    取得ページ番号
-   * @return {Array[Obejct]}      Togglのレポートオブジェクト
-   */
-  _fetchReport(workplaceId, since, until, page = 1) {
-    const url = Utilities.formatString(
-      'https://toggl.com/reports/api/v2/details?workspace_id=%s&since=%s&until=%s&page=%s&user_agent=toggl2rm',
-      workplaceId,
-      since,
-      until,
-      page,
-    );
-    const response = this._callTogglApi(url);
-    const content = JSON.parse(response.getContentText());
-    const reportJson = content.data;
-    const hasNext = (content.per_page <= reportJson.length);
-    Logger.log('%s, %s, %s', reportJson.length, hasNext, page);
-    return { reportJson, hasNext };
-  },
-
-  /**
-   * レポート（詳細）を扱いやすくパースする。
-   * レポートオブジェクトから次の値の配列に変換する
-   *   - report.id : TooglのタスクID
-   *   - ticketNo :  Redmineのチケット 番号
-   *   - startDate : 作業の日付
-   *   - duration : 作業時間（Hour）
-   *   - report.tags.join('、'): 作業のメモ（Togglのタグ）
-   *   - report.description: Togglのタスク名
-   * @param  {Array[Object]} reportJson Togglのレポートオブジェクトの配列
-   * @return {Array[][]}     必要な値の配列のリスト
-   */
-  _parseReportData(reportJson) {
-    const ticketNoIndex = 1;
-    const parsedReport = reportJson.map((report) => {
-      const startDate = /^([0-9]{4}-[0-9]{2}-[0-9]{2})T.+$/.exec(report.start)[1];
-      const duration = Math.round(report.dur / (60 * 60 * 10)) / 100;
-      let ticketNo = '';
-      if (/^#/.test(report.description)) {
-        ticketNo = /^#([0-9]+):?[\s][\S]+/.exec(report.description)[ticketNoIndex];
-      } else {
-        return null;
-      }
-      const formattedRepo = [report.id, ticketNo, startDate, duration, report.tags.join('、'), report.description];
-      return formattedRepo;
-    }).filter(elm => (elm != null));
-    return parsedReport;
-  },
-
-  /**
-   * TogglのAPIを実行する
-   * @param  {[type]} url APIのエンドポイントURL＋クエリパラメータ
-   * @return {[type]}     APIのレスポンス
-   */
-  _callTogglApi(url) {
-    const togglKey = Props.get('TOGGL_API_TOKEN');
-    const authToken = Utilities.base64Encode(`${togglKey}:api_token`);
-    const headers = { Authorization: `Basic ${authToken}` };
-    const response = UrlFetchApp.fetch(url, { headers });
-    return response;
   },
 };
 
